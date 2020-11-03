a6d45cb2fa2eef7ba4b974407ee819bb2ada6674
gokhandogan7 <gdogan3746@gmail.com>
2020-11-03 14:44:37 +0100

    CityList Created

--- src/pages/CityList.js
+++ src/pages/CityList.js
@@ -7,10 +7,27 @@ import {CityItem, SearchBar} from '../components';
 
 const CityList = (props) => {
   const [cityList, setCityList] = useState([]);
+  const fetchCityData = async () => {
+    const {data} = await axios.get('http://opentable.herokuapp.com/api/cities');
+    setCityList(data.cities);
+  };
+
+  useEffect(() => {
+    fetchCityData();
+  }, []);
+
+  const renderCities = ({item}) => <CityItem cityName={item} />;
 
   return (
     <View>
-      <Text>CityList</Text>
+      <Text style={{fontSize: 35, fontWeight: 'bold', textAlign: 'center'}}>
+        CityList
+      </Text>
+      <FlatList
+        keyExtractor={(_, index) => index.toString()}
+        data={cityList}
+        renderItem={renderCities}
+      />
     </View>
   );
 };
